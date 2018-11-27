89ef4ec8f52a0663bfbb10ef8de59d12d702cecf
martinwoell <38783218+martinwoell@users.noreply.github.com>
2018-11-27 10:33:53 +0100

    finished Aufgabe.py
    class Aufgabe.py is already finished, in this change i removed the testing code in the main class

--- WarriorGame/RunMe.py
+++ WarriorGame/RunMe.py
@@ -9,13 +9,7 @@ if __name__ == '__main__':
     name = input("Input the name of the warrior")
     print("The warriors name is " +name)
     
-    "Aufgabe wird getestet"
     
-    x = Aufgabe("Test", 1)
-    print(x.experience)
-    print(x.goldReward)
-    print(x.name)
-    print(x.questLevel)
     
     
     
