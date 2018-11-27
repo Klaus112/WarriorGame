194f7a8a5dfa2fe5ca6c19d3371bb42473aecc98
martinwoell <38783218+martinwoell@users.noreply.github.com>
2018-11-27 10:00:35 +0100

    create Aufgabe.py

---
+++ WarriorGame/Aufgabe.py
@@ -0,0 +1,17 @@
+'''
+Created on 27.11.2018
+
+@author: Martin
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
