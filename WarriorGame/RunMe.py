026df71297716c3fd2282ed9d1e0cf0a797ba2a2
Klaus112 <tusch.klaus@gmail.com>
2018-11-27 10:49:45 +0100

    Added new functions to main
    Added a mainMenu function
    Added a marketplace function

--- WarriorGame/RunMe.py
+++ WarriorGame/RunMe.py
@@ -2,9 +2,24 @@
 Created on 27.11.2018
 
 @author: Klaus
-'''
 
-if __name__ == '__main__':
+The main function of this game
+'''
+def inputName():
     name = input("Input the name of the warrior")
     print("The warriors name is " +name)
+    #placeholder Hero(name,....) erstellen
+    return name;
+
+def mainMenu():
+    print("Hello")
     
+def marketplace():
+    pass;
+
+if __name__ == '__main__':
+    inputName();
+    while(True):
+        pass
+        break;
+       
