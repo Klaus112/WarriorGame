f474f83945281bacfcaa3583c36e398140e515d6
Klaus112 <tusch.klaus@gmail.com>
2018-12-05 12:30:20 +0100

    Create Hero.py
    Denis created Hero.py but couldn't upload it so i did.
    A few setters were also created.

---
+++ WarriorGame/Hero.py
@@ -0,0 +1,24 @@
+
+
+
+class Hero:
+    def __init__(self, name):
+    
+        self.Name = name
+        self.HeroLevel = 1
+        self.WeaponLevel = 1
+        self.GoldValue = 5
+        self.Damage = 10*(self.WeaponLevel+self.HeroLevel)
+        self.Health = 22*(self.HeroLevel)
+
+def setWeaponLevel(self, value):
+    self.WeaponLevel += value
+    self.update()
+
+def setHeroLevel(self, value):
+    self.HeroLevel += value
+    self.update()
+
+def update (self):
+    self.Damage = 10*(self.WeaponLevel+self.HeroLevel)
+    self.Health = 22*(self.HeroLevel)
